2abfdc77979558b52152a699d91f7642085fd681
[Alexander Peter] <[ad8peter@gmail.com]>
2021-05-09 18:07:48 -0700

    Nearly there

--- src/main/java/com/piggybank/services/ChangeCredentials.java
+++
@@ -1,5 +0,0 @@
-package com.piggybank.services;
-
-public class ChangeCredentials {
-	
-}
--- src/main/java/com/piggybank/servlets/UserMenuServlet.java
+++ src/main/java/com/piggybank/servlets/UserMenuServlet.java
@@ -8,18 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UserMenuServlet
- */
+import com.piggybank.models.Users;
+
+
 public class UserMenuServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+      
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		Users user = new Users();
 		request.getRequestDispatcher("UserMenu.html").forward(request, response);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		PrintWriter pw = response.getWriter();
+		Users user = new Users();
+		String outputHTML = "Welcome " + user.getFirstName();
+		pw.print(outputHTML);
+		request.getRequestDispatcher("UserMenu.html").forward(request, response);
 	}
-
 }
